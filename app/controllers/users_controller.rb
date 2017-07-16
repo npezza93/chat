31fa4a05bf41061d35e0a565fa866011c21b8d6d
npezza93 <npezza93@gmail.com>
2017-07-15 22:32:26 -0400

    Fix wrong user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,8 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first, :last, :image, :email, :password)
+    params.require(:user).permit(
+      :first_name, :last_name, :image, :email, :password
+    )
   end
 end

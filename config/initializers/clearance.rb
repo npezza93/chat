2a3388809d23196af4f870aac87f236d068869f1
npezza93 <npezza93@gmail.com>
2017-07-15 21:19:36 -0400

    Add clearance

---
+++ config/initializers/clearance.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Clearance.configure do |config|
+  config.mailer_sender = "reply@example.com"
+  config.rotate_csrf_on_sign_in = true
+end
---
+++ db/migrate/20170716010934_create_users.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.timestamps null: false
+      t.string :email, null: false, index: true
+      t.string :first_name
+      t.string :last_name
+      t.attachment :image
+      t.string :chat_status, default: "offline"
+      add_tokens_password(t)
+    end
+  end
+
+  private
+
+  def add_password_tokens(table)
+    table.string :encrypted_password, limit: 128, null: false
+    table.string :confirmation_token, limit: 128
+    table.string :remember_token, limit: 128, null: false, index: true
+  end
+end

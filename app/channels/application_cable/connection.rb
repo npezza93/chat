076ed1db6dcdd0e7914414d8006744bfa91e0c65
npezza93 <npezza93@gmail.com>
2017-07-15 21:51:25 -0400

    Add identified_by for action cable connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,24 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    protected
+
+    def find_verified_user
+      if (current_user = User.find_by(remember_token: token))
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
+    def token
+      request.cookie_jar[:remember_token]
+    end
   end
 end
